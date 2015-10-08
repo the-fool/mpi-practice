911afb2535e9a5a8a525d1fd8811caf2d3b4907c
Thomas Ruble <sketchbang@gmail.com>
2015-10-07 19:39:28 -0500

    recv from any source

--- heyworld.c
+++ heyworld.c
@@ -15,14 +15,13 @@ int main ( int argc, char **argv )
   gethostname(hostname, 255);
 
   if (rank != 0) {
-    printf("HELLO!\n");
-    sprintf(msg, "I am rank %d", rank);
+    sprintf(msg, "I am rank %d on host %s", rank, hostname);
     MPI_Send(msg, strlen(msg)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);    
   } else {
     printf("Hey there, I am number %d on host %s\n", rank, hostname);
     int i;
     for(i = 1; i < comm_sz; i++) {
-      MPI_Recv(msg, 128, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(msg, 128, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       printf("%s\n", msg); 
     } 
   }
